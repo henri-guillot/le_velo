df71c215a89c8c3fa47a4981b03bff855287ba68
henri guillot <henri.guillot@hotmail.com>
2023-12-20 19:17:02 +0100

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bikes, dependent: :destroy
+
+  has_one_attached :photo, dependent: :destroy
+
+  validates :username, presence: true
+
 end
