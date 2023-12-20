ad0e65c1924678c719d784062f3138861fe0a8e9
henri guillot <henri.guillot@hotmail.com>
2023-12-20 19:29:00 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :bikes do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy] do
+    member do
+      patch :accept
+      patch :reject
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
