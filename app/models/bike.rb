e119417f6fc70a6134daf5d20617448a9ba5b914
henri guillot <henri.guillot@hotmail.com>
2023-12-20 19:22:15 +0100

    model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,10 @@
 class Bike < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
+
+  validates :style, presence: true, length: { minimum: 6 }
+  validates :short_description, presence: true, length: { minimum: 5 }
+  validates :long_description, presence: true, length: { minimum: 20 }
+  validates :price_per_day, presence: true, numericality: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
+  has_many_attached :photos
+
+  validates :start_date, :status, presence: true
+  validates :end_date, presence: true, comparison: { greater_than: :start_date }
+  validates :total_price, presence: true, numericality: true
 end
