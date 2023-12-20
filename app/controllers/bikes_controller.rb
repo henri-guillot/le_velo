16ddf319acfcaae463430b57aab4142fef44c68a
henri guillot <henri.guillot@hotmail.com>
2023-12-20 21:38:11 +0100

    commited changes to bike controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,8 +1,69 @@
 class BikesController < ApplicationController
+  # before_action :set_bike, only: [:show, :edit, :update, :destroy]
+
+  # def index
+  #   @bikes = Bike.all
+  # end
+
+  # def new
+  #   @bike = Bike.new
+  # end
+
+  # def create
+  #   @bike = Bike.new(bike_params)
+  #   if @bike.save
+  #     redirect_to bikes_path
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def show
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  #   if @bike.update(bike_params)
+  #     redirect_to bike_path(@bike)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def destroy
+  #   @bike.destroy
+  #   redirect_to bikes_path, status: :see_other
+  # end
+
+  # private
+
+  # def set_bike
+  #   @bike = Bike.find(params[:id])
+  # end
+
+  # def bike_params
+  #   params.require(:bike).permit(:name, :place, :description, :price_per_day, :bike_type, :photo)
+  # end
   before_action :set_bike, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @bikes = Bike.all
+    @bikes = Bike.where(user: current_user)
+
+    @all_bikes = Bike.all # Attention aux requêtes N+1
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        bikes.style @@ :query
+        OR bikes.LONG_description @@ :query
+      SQL
+      @all_bikes = @all_bikes.where(sql_subquery, query: params[:query])
+    end
+  end
+
+  def show
+    @booking = Booking.new
   end
 
   def new
@@ -11,14 +72,12 @@ class BikesController < ApplicationController
 
   def create
     @bike = Bike.new(bike_params)
+    @bike.user = current_user
     if @bike.save
-      redirect_to bikes_path
+      redirect_to bike_path(@bike)
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def show
   end
 
   def edit
@@ -28,7 +87,7 @@ class BikesController < ApplicationController
     if @bike.update(bike_params)
       redirect_to bike_path(@bike)
     else
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -44,6 +103,8 @@ class BikesController < ApplicationController
   end
 
   def bike_params
-    params.require(:bike).permit(:name, :place, :description, :price_per_day, :bike_type, :photo)
+    params.require(:bike).permit(:style, :description, :price_per_day, photos: [])
   end
+
+
 end
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,9 +3,9 @@ class Bike < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
 
-  validates :name, presence: true
-  validates :style, presence: true, length: { minimum: 6 }
-  validates :short_description, presence: true, length: { minimum: 5 }
-  validates :long_description, presence: true, length: { minimum: 20 }
-  validates :price_per_day, presence: true, numericality: true
+  # validates :name, presence: true
+  # validates :style, presence: true, length: { minimum: 6 }
+  # validates :short_description, presence: true, length: { minimum: 5 }
+  # validates :long_description, presence: true, length: { minimum: 20 }
+  # validates :price_per_day, presence: true, numericality: true
 end
