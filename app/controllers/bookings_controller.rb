c17bb99b3e7075b749108600bfa6653309efba10
henri guillot <henri.guillot@hotmail.com>
2023-12-21 08:55:39 +0100

    commit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,53 @@
 class BookingsController < ApplicationController
+  before_action :set_bike, only: [:create]
+
+  def index
+    @bookings = Booking.where(user: current_user)
+    @my_booked_bikes = Booking.where(bike: Bike.where(user: current_user))
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.bike = @bike
+    @booking.status = "Pending"
+    @booking.total_price = (@booking.end_date - @booking.start_date) * @bike.price_per_day
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render partial: "form", locals: { bike: @bike, booking: @booking }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "Accepted")
+    redirect_to booking_path(@booking)
+  end
+
+  def reject
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "Rejected")
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:bike_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
